8bbd3540e26720cb0ff3f085d2781f5693062a46
MarcelArt <badman.islemon@gmail.com>
2025-10-12 01:55:56 +0700

    feat - receive payment

--- src/selling_phase.rs
+++ src/selling_phase.rs
@@ -1,4 +1,4 @@
-use godot::{classes::{INode2D, Label, Node2D}, prelude::*};
+use godot::{classes::{INode2D, Node2D, RichTextLabel}, prelude::*};
 
 use crate::customer::Customer;
 
@@ -12,13 +12,15 @@ struct CustomerOrder {
 #[class(base=Node2D)]
 pub struct SellingPhase {
     base: Base<Node2D>,
-    clock_label: Option<Gd<Label>>,
+    clock_label: Option<Gd<RichTextLabel>>,
+    money_label: Option<Gd<RichTextLabel>>,
     current_time: f64,
     end_time: f64,  // End time in minutes (e.g., 20:00 = 1200 minutes)
     is_day_over: bool,
     time_speed: f64, // Time speed multiplier
     serving_speed: f32,
     orders: Vec<CustomerOrder>,
+    money: i32,
     
     // Change or add your own properties here
     #[export]
@@ -31,6 +33,7 @@ impl INode2D for SellingPhase {
         Self {
             base,
             clock_label: None,
+            money_label: None,
             current_time: (8 * 60) as f64, // Start at 8:00 AM
             time_speed: 1 as f64, // Normal speed
             end_time: (17 * 60) as f64, // End at 8:00 PM
@@ -38,11 +41,16 @@ impl INode2D for SellingPhase {
             time_multiplier: 5 as f64, // Default time multiplier
             serving_speed: 1.0, // Default serving speed
             orders: Vec::new(),
+            money: 0,
         }
     }
 
     fn ready(&mut self) {
-        self.clock_label = Some(self.base().get_node_as("UI/ClockLabel"));
+        self.clock_label = Some(self.base().get_node_as("UI/VBoxContainer/ClockLabel"));
+        
+        self.money_label = Some(self.base().get_node_as("UI/VBoxContainer/MoneyLabel"));
+        let text = format!("Rp. {}", self.money);
+        self.money_label.as_mut().unwrap().set_text(&text);
     }
 
     fn process(&mut self, _delta: f64) {
@@ -78,19 +86,32 @@ impl SellingPhase {
     }
 
     fn serve_customer(&mut self, delta: f64) {
-        let order = self.orders.get_mut(0);
-        if order.is_none() {
-            return;
-        }
-        let order = order.unwrap();
+        let mut paid_amount = 0;
         
-        let progress = delta as f32 * self.serving_speed;
-        order.progress += progress;
-        if order.progress >= order.amount as f32 { // order complete remove order queue and change customer state
-            godot_print!("Served customer {}", order.customer.get_name());
-            order.customer.bind_mut().complete_order();
-            godot_print!("Customer paid: {}", order.amount * 8000);
-            self.orders.remove(0);
+        // mutable borrow self
+        {
+            let order = self.orders.get_mut(0);
+            if order.is_none() {
+                return;
+            }
+            let order = order.unwrap();
+            
+            let progress = delta as f32 * self.serving_speed;
+            order.progress += progress;
+            if order.progress >= order.amount as f32 { // order complete remove order queue and change customer state
+                godot_print!("Served customer {}", order.customer.get_name());
+                order.customer.bind_mut().complete_order();
+                paid_amount = order.amount * 8000;
+                self.orders.remove(0);
+            }
         }
+        
+        self.getting_paid(paid_amount);
+    }
+
+    fn getting_paid(&mut self, amount: i32) {
+        self.money += amount;
+        let text = format!("Rp. {}", self.money);
+        self.money_label.as_mut().unwrap().set_text(&text);
     }
 }
