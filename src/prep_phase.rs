7121420ce45e709f5b59b514a096d080fee39ff7
MarcelArt <badman.islemon@gmail.com>
2025-10-15 23:27:42 +0700

    wip - prep ui functionality

--- src/prep_phase.rs
+++ src/prep_phase.rs
@@ -1,16 +1,22 @@
-use godot::{classes::{Button, Control, Engine, IControl, LineEdit}, prelude::*};
+use godot::{classes::{Button, Control, Engine, IControl, RichTextLabel}, prelude::*};
 
 use crate::singletons::game_data::GameDataSingleton;
+
 
 #[derive(GodotClass)]
 #[class(base=Control)]
 struct PrepPhase {
     base: Base<Control>,
-    start_day_button: Option<Gd<Button>>,
-    stock_input: Option<Gd<LineEdit>>,
-
+    
     // Change or add your own properties here
-    // #[export]
+    #[export]
+    start_day_button: Option<Gd<Button>>,
+    #[export]
+    save_recipe_button: Option<Gd<Button>>,
+    #[export]
+    money_label: Option<Gd<RichTextLabel>>,
+    #[export]
+    day_count_label: Option<Gd<RichTextLabel>>,
 }
 
 #[godot_api]
@@ -19,19 +25,34 @@ impl IControl for PrepPhase {
         Self {
             base,
             start_day_button: None,
-            stock_input: None,
+            save_recipe_button: None,
+            money_label: None,
+            day_count_label: None,
         }
     }
 
     fn ready(&mut self) {
-        self.start_day_button = Some(self.base().get_node_as("StartDayButton"));
+        let mut game_data: Gd<GameDataSingleton> = Engine::singleton().get_singleton(&StringName::from("GameDataSingleton")).unwrap().cast();
+        if game_data.bind().is_new_game() {
+            game_data.bind_mut().start_new();
+        }
+        
+        let money = game_data.bind().money;
+        let day = game_data.bind().day;
+        self.money_label.as_mut().unwrap().set_text(&money.to_string());
+        self.day_count_label.as_mut().unwrap().set_text(format!("Day {}", day).as_str());
+
         let start_day_button = self.start_day_button.as_ref().unwrap();
         start_day_button
             .signals()
             .pressed()
             .connect_other(&*self, Self::_on_start_day_button_pressed);
 
-        self.stock_input = Some(self.base().get_node_as("StockInput"));
+        let save_recipe_button = self.save_recipe_button.as_ref().unwrap();
+        save_recipe_button
+            .signals()
+            .pressed()
+            .connect_other(&*self, Self::_on_save_recipe_button_pressed);
     }
 
     fn process(&mut self, _delta: f64) {
@@ -41,10 +62,15 @@ impl IControl for PrepPhase {
 
 impl PrepPhase {
     fn _on_start_day_button_pressed(&mut self) {
-        let mut game_data: Gd<GameDataSingleton> = Engine::singleton().get_singleton(&StringName::from("GameDataSingleton")).unwrap().cast();
-        game_data.bind_mut().stock = self.stock_input.as_ref().unwrap().get_text().to_int() as i32;
+        // let mut game_data: Gd<GameDataSingleton> = Engine::singleton().get_singleton(&StringName::from("GameDataSingleton")).unwrap().cast();
 
         let mut tree = self.base().get_tree().unwrap();
         tree.change_scene_to_file("res://scenes/selling_phase.tscn");
+    }
+
+    fn _on_save_recipe_button_pressed(&mut self) {
+        let game_data: Gd<GameDataSingleton> = Engine::singleton().get_singleton(&StringName::from("GameDataSingleton")).unwrap().cast();
+        godot_print!("{}", game_data.bind().money);
+        // Save recipe logic here
     }
 }
--- src/singletons/game_data.rs
+++ src/singletons/game_data.rs
@@ -6,9 +6,28 @@ pub struct GameDataSingleton {
     base: Base<Object>,
     pub stock: i32,
     pub money: i32,
-
+    pub day: i32,
+    pub coffee: i32,
+    pub milk: i32,
+    pub sugar: i32,
+    pub cup: i32,
+    pub price: i32,
 }
 
-// #[godot_api]
-// impl GameDataSingleton {
-// }
+#[godot_api]
+impl GameDataSingleton {
+    pub fn start_new(&mut self) {
+        self.stock = 0;
+        self.money = 5_000_000;
+        self.day = 1;
+        self.coffee = 0;
+        self.milk = 0;
+        self.sugar = 0;
+        self.cup = 0;
+        self.price = 8000;
+    }
+
+    pub fn is_new_game(&self) -> bool {
+        self.day == 0
+    }
+}
